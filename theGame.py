56717225497f967529012df7bd56a15988874142
JanoHorvath <jano.horvath@gmail.com>
2015-07-05 11:28:00 +0200

    Clicking on canvas now counts as a player1 move. Created new graphical game engine class. Introduced new custom exceptions: InvalidMove and GameOver.

--- theGame.py
+++ theGame.py
@@ -2,6 +2,13 @@ import Tkinter
 import horizontal_ai    as ai1
 import random_ai        as ai2
 
+class GameOver(Exception):
+    pass
+
+
+class InvalidMove(Exception):
+    pass
+
 
 class Player:
     """ Simple player object. Stores name & symbol attributes."""
@@ -28,10 +35,7 @@ class TheGame:
         if self.showVisual:
             self.tk_app = Tkinter.Tk()
             self.init_graphics(self.tk_app)
-
-        if self.showVisual:
             self.tk_app.mainloop()
-
         else:
             print 'The game begins! Brace yourselves.'
             self.game_engine(self.player1, self.player2)  # Player1 starts
@@ -41,32 +45,43 @@ class TheGame:
         self.canvas = Tkinter.Canvas(tk_app, height=500, width=500)
         self.canvas.grid(row=0, column=0, rowspan=10)
 
-        description_label = Tkinter.Label(tk_app,
-                                    text="This is TIC TAC TOE",
-                                    justify='left').grid(row=0, column=1, sticky='s')
-
-        this = self
-
-        def player1_move(this):
+        def human_move(event):
             try:
-                self.game_engine(this.player1, this.player2)
-            except ValueError:
+                x = event.x - event.x%10
+                y = event.y - event.y%10
+                self.graphic_game_engine(self.player1, (x,y))
+            except InvalidMove:
+                print 'Invalid move! Try again!'
+            except GameOver:
+                print "Game finished."
+                tk_app.destroy()
+
+        self.canvas.bind("<Button-1>", human_move)
+
+        def ai1_move(this):
+            try:
+                self.graphic_game_engine(self.player1, None)
+            except GameOver:
                 print "Game finished."
                 this.tk_app.destroy()
 
-        def player2_move(this):
+        def ai2_move(this):
             try:
-                self.game_engine(this.player2, this.player1)
-            except ValueError:
+                self.graphic_game_engine(self.player2, None)
+            except GameOver:
                 print "Game finished."
                 this.tk_app.destroy()
 
         player1_move_button = Tkinter.Button(tk_app,
                                                   text="AI1 make a move",
-                                                  command=lambda: player1_move(self)).grid(row=1,column=1)
+                                                  command=lambda: ai1_move(self)).grid(row=1, column=1)
         player2_move_button = Tkinter.Button(tk_app,
                                                   text="AI2 make a move",
-                                                  command=lambda: player2_move(self)).grid(row=2,column=1)
+                                                  command=lambda: ai2_move(self)).grid(row=2, column=1)
+
+        description_label = Tkinter.Label(tk_app,
+                                    text="This is TIC TAC TOE",
+                                    justify='left').grid(row=0, column=1, sticky='s')
 
     def whats_on(self, x, y):
         """ Returns symbol on (x, y) if there is one, None otherwise. """
@@ -85,7 +100,7 @@ class TheGame:
 
     def the_end(self, winner):
         print "[ {0} ] won! End of game :)".format(winner.name)
-        raise ValueError('End of game')
+        raise GameOver()
 
     def check_win(self, player, x, y):
         """ Checks, whether x,y is a winning move for player"""
@@ -151,11 +166,23 @@ class TheGame:
 
         self.check_win(current_player, x, y)
 
-        if self.showVisual:
-            self.render()
+        self.game_engine(other_player, current_player)
+
+    def graphic_game_engine(self, current_player, human_move):
+        if human_move:
+            if self.whats_on(human_move[0], human_move[1]) is None:
+                raise InvalidMove('Try again!')
+            else:
+                x = human_move[0]
+                y = human_move[1]
         else:
-            # If playing without graphics, recursively alternates between moves.
-            self.game_engine(other_player, current_player)
+            while True:
+                x, y = current_player.ai.move(self.board)
+                if self.whats_on(x, y) is None: break
+
+        self.make_move(current_player, x, y)
+
+        self.check_win(current_player, x, y)
 
     def render(self):
         for field in self.board:
@@ -167,5 +194,5 @@ def run_it(repetitions, showVisual):
     for i in range(1, repetitions+1):
         try:
             TheGame(showVisual)
-        except ValueError:
+        except GameOver:
             print "Game {0} finished. ".format(i)
